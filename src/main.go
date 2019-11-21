00b83d80defec2ddb737c315a979919e143c28c8
hyx <754060604@qq.com>
2019-11-21 09:26:05 +0800

    fix 统计数据不准确的问题

--- src/main.go
+++ src/main.go
@@ -6,65 +6,33 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var (
-	c = flag.Int("c", 0, "Plz input client quantity")
-	t = flag.Int("t", 0, "Plz input times quantity")
-	u = flag.String("u", "", "Plz input url")
+	c = flag.Int("c", 0, "请输入请求数量")
+	t = flag.Int("t", 0, "请输入线程数量")
+	u = flag.String("u", "", "请输入测试地址")
 )
 
-var (
-	total   = 0.0
-	about   = 0.0
-	success = 0.0
-	failure = 0.0
-	useTime = 0.0
-	index   = 0
-)
-
-var wg sync.WaitGroup
-
-func run(num int) {
-
-	defer wg.Done()
-
-	no := 0.0
-	ok := 0.0
-
-	for i := 0; i < num; i++ {
-		start := time.Now()
-		resp, err := http.Get(*u)
-		index++
-
-		if err != nil {
-			no += 1
-			log.Printf("%d	%s	请求耗时：%.4fs	Error:%s\n", index, *u, time.Since(start).Seconds(), err.Error())
-			continue
-		}
-		//log.Printf("#%d	%s	请求耗时：%.4fs	HttpCode:%s\n", index, *u, time.Since(start).Seconds(), resp.Status)
-
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			no += 1
-			continue
-		}
-
-		ok += 1
-		continue
-	}
-
-	success += ok
-	failure += no
-	total += float64(num)
-
+type Result struct {
+	PreTotal int
+	Total    int64
+	Success  int64
+	Failure  int64
+	Error    int64
+	Index    int64
 }
+
+var result Result
+
+var startTime = time.Now().UnixNano()
+var wg sync.WaitGroup
 
 func main() {
 
-	startTime := time.Now().UnixNano()
+	startTime = time.Now().UnixNano()
 
 	flag.Parse()
 
@@ -73,21 +41,56 @@ func main() {
 		return
 	}
 
-	for i := 0; i < *c; i++ {
+	for i := 0; i < *t; i++ {
 		wg.Add(1)
-		go run(*t)
+		go run(&i, *c)
 	}
 
 	wg.Wait()
-	endTime := time.Now().UnixNano()
-	_time := float64(endTime-startTime) / 1e9
-	count := total / _time
+	time.Sleep(time.Duration(2) * time.Second)
+	printInfo()
+}
 
+func run(cIndex *int, num int) {
+	for i := 0; i < num; i++ {
+		atomic.AddInt64(&result.Total, 1)
+		request(*u, &i, cIndex)
+	}
+	defer wg.Done()
+}
+
+func request(url string, i, c *int) {
+	atomic.AddInt64(&result.Index, 1)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		atomic.AddInt64(&result.Error, 1)
+		log.Println(err, i, c)
+	} else if resp.StatusCode != 200 {
+		atomic.AddInt64(&result.Failure, 1)
+		defer resp.Body.Close()
+	} else {
+		atomic.AddInt64(&result.Success, 1)
+		defer resp.Body.Close()
+	}
+
+}
+
+func printInfo() {
+
+	endTime := time.Now().UnixNano()
+	_time := float64(endTime-startTime)/1e9 - 2
+	count := float64(result.Total) / _time
+	rate := float64(result.Success) / float64(result.Total) * 100.0
+
+	fmt.Println("-----------------------------------------------")
 	fmt.Println("应请求总数:    ", (*c)*(*t))
-	fmt.Println("实际请求总数:  ", total)
-	fmt.Println("成功:          ", success)
-	fmt.Println("失败:          ", failure)
-	fmt.Println("成功率:        ", fmt.Sprintf("%.2f", (success/total)*100.0), "%")
-	fmt.Println("总耗时:        ", fmt.Sprintf("%.4f", _time), "s")
+	fmt.Println("实际请求总数:  ", result.Total)
+	fmt.Println("成功:          ", result.Success)
+	fmt.Println("失败:          ", result.Failure)
+	fmt.Println("错误:          ", result.Error)
+	//fmt.Println("丢失:          ", result.Total-(result.Success+result.Failure+result.Error))
+	fmt.Println("成功率:        ", fmt.Sprintf("%.2f%s", rate, "%"))
+	fmt.Println("总耗时:        ", fmt.Sprintf("%.4fs", _time))
 	fmt.Println("每秒请求数：   ", fmt.Sprintf("%.0f", count))
 }
